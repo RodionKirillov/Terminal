516576e18b699ee5c2470c7ad8ca469839cbf32a
RodionKirillov <rodion.44454@gmail.com>
2024-09-16 16:58:51 +0300

    10.7

--- app/src/main/java/com/example/terminal/data/ApiService.kt
+++ app/src/main/java/com/example/terminal/data/ApiService.kt
@@ -4,6 +4,6 @@ import retrofit2.http.GET
 
 interface ApiService {
 
-    @GET("v2/aggs/ticker/AAPL/range/1/hour/2023-09-16/2024-09-16?adjusted=true&sort=asc&limit=50000&apiKey=yQI2Yyniqe_xaZppzsoPr7nlPhPqqVmK")
+    @GET("v2/aggs/ticker/AAPL/range/1/hour/2023-09-16/2024-09-16?adjusted=true&sort=desc&limit=50000&apiKey=yQI2Yyniqe_xaZppzsoPr7nlPhPqqVmK")
     suspend fun loadBars(): Result
 }
